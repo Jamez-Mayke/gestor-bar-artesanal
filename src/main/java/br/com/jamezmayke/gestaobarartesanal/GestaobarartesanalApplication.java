133fb732cf295d429267888d0e6d5a1c5e50cb9d
jamesmayke <jamezmayke28@gmail.com>
2026-05-06 19:48:12 -0300

    chore: estrutura inicial do projeto

---
+++ src/main/java/br/com/jamezmayke/gestaobarartesanal/GestaobarartesanalApplication.java
@@ -0,0 +1,13 @@
+package br.com.jamezmayke.gestaobarartesanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaobarartesanalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaobarartesanalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/jamezmayke/gestaobarartesanal/GestaobarartesanalApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jamezmayke.gestaobarartesanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaobarartesanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
